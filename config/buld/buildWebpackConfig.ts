a6cdc3ce8ca01214e8c866a2995787b879fef8f3
Andrew <asoborev@mail.ru>
2024-12-23 23:44:14 +0300

    форматирование кода

--- config/buld/buildWebpackConfig.ts
+++ config/buld/buildWebpackConfig.ts
@@ -1,9 +1,9 @@
 import {BuildOptions} from "./types/config";
 import webpack from "webpack";
-import {buildPlugins} from "./buildPlugins";
-import {buildLoaders} from "./buildLoaders";
-import {buildResolves} from "./buildResolves";
-import {buildDevServer} from "./buildDevServer";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolves } from "./buildResolves";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
     const {mode, paths, isDev} = options;
