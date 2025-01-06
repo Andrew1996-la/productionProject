6379f87e158881c93e11ce3638973a19082faf3d
Andrew <asoborev@mail.ru>
2025-01-06 19:38:43 +0300

    navbar был вынесен как widget, Link вынес в shared

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
 import './styles/index.scss';
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/themeProvider";
 import {AppRoutes} from "app/providers/routes";
+import {Navbar} from "widgets/Navbar";
 
 export function App() {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Link to='/'>Main page</Link>
-            <Link to='/about'>About page</Link>
+            <Navbar />
             <button onClick={toggleTheme}>toggle theme</button>
             <AppRoutes />
         </div>
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import {FC} from "react";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './navbar.module.scss';
+import {AppLink} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    classes?: string
+}
+
+export const Navbar:FC<NavbarProps> = (props) => {
+    const {classes} = props;
+
+    return (
+        <div className={classNames(cls.navbar, {}, [classes])}>
+            <nav className={classNames(cls.links)}>
+                <AppLink to='/'>Main page</AppLink>
+                <AppLink to='/about'>About page</AppLink>
+            </nav>
+        </div>
+    );
+};
