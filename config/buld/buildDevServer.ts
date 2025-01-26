9a315edfb9b28942b0c71166dd3594af35163789
Andrew <asoborev@mail.ru>
2025-01-26 21:43:10 +0300

    Добавлены плагины в webpack для hot reload

--- config/buld/buildDevServer.ts
+++ config/buld/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buildDevServer({port}: BuildOptions): webpackDevServer.Configura
         port: port,
         open: true,
         historyApiFallback: true,
+        hot: true,
     }
 }
--- config/buld/buildPlugins.ts
+++ config/buld/buildPlugins.ts
@@ -1,7 +1,8 @@
-import webpack, { DefinePlugin } from "webpack";
+import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
@@ -13,8 +14,10 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
-        new DefinePlugin({
+        new webpack.DefinePlugin({
             __IS_DEV__: isDev
         }),
+        isDev && new webpack.HotModuleReplacementPlugin(),
+        isDev && new ReactRefreshWebpackPlugin(),
     ]
 }
