4d72bc8fd830d56ffa7eadcb7a2a742b279d65c5
Andrew <asoborev@mail.ru>
2025-06-04 09:32:08 +0300

    Протестировал базовый компонент Button

--- src/shared/ui/Button/ui/Button.test.tsx
+++ src/shared/ui/Button/ui/Button.test.tsx
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
-test('Button test', () => {
-    render(<Button>Test</Button>)
-    expect(screen.getByText('Test')).toBeInTheDocument();
+import { Button, ButtonTheme } from './Button';
+
+describe('Button test', () => {
+    test('Default Button', () => {
+        render(<Button>Test</Button>)
+        expect(screen.getByText('Test')).toBeInTheDocument();
+    })
+    test('Button with theme', () => {
+        render(<Button theme={ButtonTheme.CLEAR}>Test</Button>)
+        expect(screen.getByText('Test')).toHaveClass('clear');
+        screen.debug();
+    })
 })
