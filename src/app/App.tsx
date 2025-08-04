49888fd40514aecb05143160d4d23ce82550ad77
Andrew <asoborev@mail.ru>
2025-08-04 18:57:42 +0300

    тестирование github actions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { AppRoutes } from 'app/providers/routes';
 import { useTheme } from 'app/providers/themeProvider';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
@@ -9,12 +9,6 @@ import './styles/index.scss';
 
 export function App() {
     const { theme } = useTheme();
-
-    useEffect(() => {
-        if (Math.random() > 0.5) {
-            throw new Error();
-        }
-    }, []);
 
     return (
         <div className={classNames('app', {}, [theme])}>
