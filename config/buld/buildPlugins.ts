d7bfb983c49bc31d99e5ae1bfef9a35067c6b494
Andrew <asoborev@mail.ru>
2025-08-05 22:54:29 +0300

    рефакторинг плагинов

--- config/buld/buildPlugins.ts
+++ config/buld/buildPlugins.ts
@@ -6,22 +6,49 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
         new webpack.DefinePlugin({
-            __IS_DEV__: isDev,
-        }),
-        isDev && new webpack.HotModuleReplacementPlugin(),
-        isDev && new ReactRefreshWebpackPlugin(),
-        isDev && new BundleAnalyzerPlugin({
-            openAnalyzer: false,
+            __IS_DEV__: JSON.stringify(isDev),
         }),
     ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }));
+    }
+
+    return plugins;
 }
+
+// const plugins = [
+//     new HtmlWebpackPlugin({
+//         template: paths.html,
+//     }),
+//     new webpack.ProgressPlugin(),
+//     new MiniCssExtractPlugin({
+//         filename: 'css/[name].[contenthash:8].css',
+//         chunkFilename: 'css/[name].[contenthash:8].css',
+//     }),
+//     new webpack.DefinePlugin({
+//         __IS_DEV__: JSON.stringify(isDev),
+//     }),
+// ];
+//
+// if (isDev) {
+//     plugins.push(new webpack.HotModuleReplacementPlugin());
+//     plugins.push(new BundleAnalyzerPlugin({
+//         openAnalyzer: false,
+//     }));
+// }
+//
+// return plugins;
