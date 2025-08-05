c2d96c6a965485a2630e58ed302557b38dcada59
Andrew <asoborev@mail.ru>
2025-08-05 22:57:46 +0300

    исправлен путь до стилей NotFound page

--- src/pages/NotFound/ui/NotFound.tsx
+++ src/pages/NotFound/ui/NotFound.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import cls from './NotFound.module.scss';
+import cls from './notFound.module.scss';
 
 const NotFound = () => {
     const { t } = useTranslation();
